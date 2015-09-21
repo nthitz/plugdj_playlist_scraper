a94858dcc29ed32996a291758457e61ef7fd70de
Nick Yahnke <nthitz@gmail.com>
2015-09-20 17:30:05 -0700

    updated delay, added message

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 (function () {
   var statusDOM = null;
+  var API_DELAY = 5000;
   function injectJqueryAndRun() {
     // the minimum version of jQuery we want
     var v = "1.3.2";
@@ -32,7 +33,7 @@
       "<textarea id='scraped_status' rows='20' cols='50'>initalizing</textarea>" +
       "<div id='scraped_playlists_submit'><input type='button' value='POST to:' id='scraped_playlists_post_button'/>" +
       "<input type='text' value='http://ks3278471.kimsufi.com:8765/archive' id='scraped_playlists_post_url' />" +
-      "</div></div>" +
+      "<br /> PLEASE CONSIDER CLICKING ABOVE BUTTON TO SAVE YOUR DATA TO MY SERVER</div></div>" +
       "<style type='text/css'>" +
       "#scraped_playlists_window { padding: 2em; position: fixed; width: 400px; height: 300px; top: 100px; left: 300px; background-color: rgba(255,255,255, 0.3); cursor: pointer; z-index: 900; }" +
       "#scraped_playlists textarea { color: black; font-family: monospace; }" +
@@ -48,16 +49,17 @@
   function close() {
     $('#scraped_playlists_window').remove()
   }
+  var totalCount = 0;
   function scrape() {
     addStatusDOM()
     $.ajax('https://plug.dj/_/playlists', {
       dataType: 'json',
     }).done(function(data) {
       var playlists = data.data;
-
+      totalCount = playlists.length
       setTimeout(function() {
         scrapePlaylist(playlists)
-      }, 750)
+      }, API_DELAY)
     })
   }
   function updateStatus(text) {
@@ -66,7 +68,7 @@
   var scrapedLists = []
   function scrapePlaylist(playlists) {
     var playlist = playlists.shift()
-    updateStatus('scraping ' + playlist.name)
+    updateStatus('scraping ' + playlist.name + ' (' + (scrapedLists.length + 1) + ' of ' + totalCount + ')')
     $.ajax('https://plug.dj/_/playlists/' + playlist.id + '/media', {
       datatype: 'json',
     }).done(function(data) {
@@ -76,7 +78,7 @@
       if (playlists.length > 0) {
         setTimeout(function() {
           scrapePlaylist(playlists)
-        }, 750)
+        }, API_DELAY)
       } else {
         end()
       }
@@ -111,22 +113,3 @@
 
 
 })();
-
-/*
-
-var playlistJson = JSON.stringify({ playlist }, null, 4);
-
-$("body").append(`
-<div id='scraped_playlists_window'>
-<div id='scraped_playlists' style=''>
-<textarea name="scraped_playlists_textarea" rows="20" cols="50">` + playlistJson + `</textarea>
-</div>
-<style type='text/css'>
-#scraped_playlists { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255); cursor: pointer; z-index: 900; }
-#scraped_playlists textarea { color: black; sans-serif; position: absolute; top: 10%; left: 10%; }
-</style>
-</div>`
-);
-
-$("#scraped_playlists").fadeIn(750);
-*/
